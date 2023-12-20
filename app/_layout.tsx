758f549b11d7ba09f803c5e00d19227b7680dd25
ylmourab77 <ylmourab.lmourabiti@gmail.com>
2023-12-20 23:26:19 +0100

    implementation Clerk Auth

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,6 +4,29 @@ import { useFonts } from 'expo-font';
 import { SplashScreen, Stack } from 'expo-router';
 import { useEffect } from 'react';
 import { useColorScheme } from 'react-native';
+import * as SecureStore from 'expo-secure-store';
+import { useRouter } from 'expo-router';
+import { ClerkProvider, useAuth } from '@clerk/clerk-expo';
+
+
+const CLERK_PUBLISHABLE_KEY ='pk_test_bWVldC1tYXN0b2Rvbi05OC5jbGVyay5hY2NvdW50cy5kZXYk'
+const tokenCache = {
+  async getToken(key:string){
+    try{
+      return SecureStore.getItemAsync(key);
+    }catch(err){
+      return null
+    }
+  },
+  async saveToken(key:string,value:string){
+    try{
+      return SecureStore.setItemAsync(key,value);
+    }catch(err){
+      console.log(err);
+    }
+  }
+
+}
 
 export {
   // Catch any errors thrown by the Layout component.
@@ -39,11 +62,20 @@ export default function RootLayout() {
     return null;
   }
 
-  return <RootLayoutNav />;
+  return (<ClerkProvider publishableKey={CLERK_PUBLISHABLE_KEY} tokenCache={tokenCache}> <RootLayoutNav />;   </ClerkProvider> )
 }
 
 function RootLayoutNav() {
+  const router = useRouter();
   const colorScheme = useColorScheme();
+  const {isLoaded, isSignedIn} = useAuth();
+
+  useEffect(() => {
+    if(!isLoaded && !isSignedIn) {
+      router.push('/(modals)/login');
+    }
+    
+  }, [isLoaded]);
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
